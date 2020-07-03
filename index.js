fdf23c0c8eb476392385cfe4ea61ae3af862f20a
kchohee <kchohee0630@gmail.com>
2020-07-03 10:50:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, funCall){
+    let newArr = []
+    array.forEach(e => {
+        newArr.push(funCall(e))
+    })
+    return newArr;
+}
+
+function reduce(array, funCall , start){
+    let newArr = (start) ? start : array[0]
+    let i = (start) ? 0 : 1
+
+    for (; i < array.length; i++ ){
+        newArr = funCall (array[i], newArr)
+    }
+    return newArr;
+} 
